e38d98e519be94adf0564a5e1cdd5f77471ad701
Darkzwer <52218993+Darkzwer@users.noreply.github.com>
2022-07-07 20:42:02 +0300

    hotfix

--- Gismart Test Task/Extensions/Timer_Animation.swift
+++ Gismart Test Task/Extensions/Timer_Animation.swift
@@ -53,17 +53,28 @@ extension ViewController {
     
 }
 
-//MARK: - FadeExtension
-//extension UIView {
-//    func fadeTransition(_ duration:CFTimeInterval) {
-//        let animation = CATransition()
-//        animation.timingFunction = CAMediaTimingFunction(name:
-//                                                            CAMediaTimingFunctionName.easeInEaseOut)
-//        animation.type = CATransitionType.fade
-//        animation.duration = duration
-//        layer.add(animation, forKey: CATransitionType.fade.rawValue)
-//    }
-//}
+//MARK: - FadeAnimation & PushAnimation
+extension UIView {
+    func fadeTransition(_ duration:CFTimeInterval) {
+        let animation = CATransition()
+        animation.timingFunction = CAMediaTimingFunction(name:
+                                                            CAMediaTimingFunctionName.easeInEaseOut)
+        animation.type = CATransitionType.fade
+        animation.duration = duration
+        layer.add(animation, forKey: CATransitionType.fade.rawValue)
+    }
+}
+
+extension UIView {
+    func pushTransition(_ duration:CFTimeInterval) {
+        let animation:CATransition = CATransition()
+        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        animation.type = CATransitionType.push
+        animation.subtype = CATransitionSubtype.fromTop
+        animation.duration = duration
+        layer.add(animation, forKey: kCATransition)
+    }
+}
 
 //MARK: - GradientExtension
 extension UIView {
