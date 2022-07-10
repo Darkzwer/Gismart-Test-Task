ad5c1246734c66f8920d45982d5021cbe57cb913
Darkzwer <52218993+Darkzwer@users.noreply.github.com>
2022-07-11 00:45:35 +0300

    Update UIColor.swift

--- Gismart Test Task/Extensions/UIColor.swift
+++ Gismart Test Task/Extensions/UIColor.swift
@@ -12,3 +12,24 @@ extension UIColor {
         return UIColor(red: 0.745, green: 0.157, blue: 0.074, alpha: 1)
     }
 }
+
+extension UIView {
+    enum GlowEffect: Float {
+        case small = 0.4, normal = 2, big = 30
+    }
+
+    func doGlowAnimation(withColor color: UIColor, withEffect effect: GlowEffect = .normal) {
+        layer.masksToBounds = false
+        layer.shadowColor = color.cgColor
+        layer.shadowRadius = 0
+        layer.shadowOpacity = 1
+        layer.shadowOffset = .zero
+
+        let glowAnimation = CABasicAnimation(keyPath: "shadowRadius")
+        glowAnimation.fromValue = 20 // effect.rawValue
+        glowAnimation.toValue = 20
+        glowAnimation.fillMode = .removed
+        glowAnimation.repeatCount = .infinity
+        layer.add(glowAnimation, forKey: "shadowGlowingAnimation")
+    }
+}
