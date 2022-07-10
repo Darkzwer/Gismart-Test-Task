da6a76760d62d0d2e45f9975acae490de0d16f3c
Darkzwer <52218993+Darkzwer@users.noreply.github.com>
2022-07-11 00:03:18 +0300

    Create UIColor.swift

---
+++ Gismart Test Task/Extensions/UIColor.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor.swift
+//  Gismart Test Task
+//
+//  Created by Igor on 10/07/2022.
+//
+
+import UIKit
+
+extension UIColor {
+    static var yourColor:UIColor {
+        return UIColor(red: 0.745, green: 0.157, blue: 0.074, alpha: 1)
+    }
+}
